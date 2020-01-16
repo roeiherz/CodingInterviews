85c122ff1ed9eba16104ee881bcf93c22d0941b3
roeiherz <roeiherz@gmail.com>
2020-01-16 10:37:33 +0200

    coinds

---
+++ Recursions_DP/8Queens.py
@@ -0,0 +1,16 @@
+__author__ = 'roeiherz'
+
+"""
+Write an algorithm to print all ways of arranging 8 queens on 8x8 chess board so that none of them share the same
+row, column or diagonal. In this case, "diagonal" means all diagonals, not just the two that bisect the board.
+"""
+
+
+def chess(n, i=0, j=0):
+    # Stop case
+    if n == 0 and cnt == 0:
+        print(prefix)
+
+
+if __name__ == '__main__':
+    chess(8)
---
+++ Recursions_DP/Coins.py
@@ -0,0 +1,50 @@
+__author__ = 'roeiherz'
+
+"""
+Given an infinite number of quarters (25 cent), dimes (10 cent), nickels (5 cent), and pennies (1 cent),
+write code to calculate the number of ways of representing n cents.
+"""
+
+
+def coins(n, arr):
+    # Stop case
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    ways = 0
+    for i in range(0, len(arr)):
+        ways += coins(n - arr[i], arr)
+
+    return ways
+
+
+def coins_dynamic(n, arr):
+    # Stop case
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    table = [0 for k in range(n + 1)]
+    # Base case (If given value is 0)
+    table[0] = 1
+
+    for i in range(0, len(arr)):
+        for j in range(arr[i], n + 1):
+            table[j] += table[j - arr[i]]
+
+    return table[n]
+
+
+if __name__ == '__main__':
+    # print(coins(20, [20, 10, 5, 1]))
+    print(coins(4, [1, 2, 3]))
+
+    #
+    # arr = [20, 10, 5, 1]
+    # m = len(arr)
+    # n = 20
+    # x = count(arr, m, n)
+    # print (x)
