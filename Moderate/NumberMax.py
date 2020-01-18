c086214c0c294f69685e670e454961859fbf48a7
roeiherz <roeiherz@gmail.com>
2020-01-18 19:29:09 +0200

    update

---
+++ Moderate/NumberMax.py
@@ -0,0 +1,12 @@
+__author__ = 'roeiherz'
+
+"""
+Write a method that finds the max of two numbers. You should not use if-else or any other comparison operator.
+"""
+
+
+
+if __name__ == '__main__':
+    x = 5
+    y = 3
+    max_num(x, y)
---
+++ Recursions_DP/PaintFill.py
@@ -0,0 +1,12 @@
+__author__ = 'roeiherz'
+
+"""
+Implement the "paint fill" function that one might see on many image editing programs. That is, given a screen 
+(represented by a two-dimensional array of colors), a point, and a new color, fill in the surrounding area until
+the color changes from the original color.
+"""
+
+
+if __name__ == '__main__':
+    screen = np.zeros((10, 10, 3))
+
