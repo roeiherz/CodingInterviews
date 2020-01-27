9e868611154600589b0c1693fa5f7441d592675d
roeiherz <roeiherz@gmail.com>
2020-01-27 15:55:48 +0200

    update

--- Moderate/SubSort.py
+++ Moderate/SubSort.py
@@ -8,6 +8,13 @@ Example: [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19] -> (3, 9)
 
 
 def smallest_diff(A, q, p):
+    """
+    not efficient
+    :param A:
+    :param q:
+    :param p:
+    :return:
+    """
 
     if q < 0 or p > len(A):
         return False
